7bba9c24d4549ab0be10584c64c699df9264ca3b
Zerrelius <Jeremy.Held@web.de>
2024-10-01 13:06:56 +0200

    Added a complete intrudction example for simple Python Programming functions

---
+++ 01-01-datatypes.py
@@ -0,0 +1,11 @@
+# Definieren der Variablen
+name = "JD"         # String
+age = 27            # Ganzzahl
+height = 1.80       # Fließkommazahl / Float
+is_student = True   # Boolean (True or False)
+
+# Ausgabe der Variablen
+print("Name:", name)
+print("Alter:", age)
+print("Körpergröße:", height, "Meter")
+print("Ist Student:", is_student)
