eda5e216d536383b94b5a90809e5d71496cc3368
Zerrelius <Jeremy.Held@web.de>
2024-10-02 11:09:13 +0200

    Input function added and explained

---
+++ 01-08-input.py
@@ -0,0 +1,13 @@
+# Wir definieren eine Variable mit dem Namen variable
+# Verwende die Input-Funktion, umd en User aufzufordern einen Wert einzugeben
+# Der Wert der eingegeben wurde, wird dann in der Variable variable gespeichert
+variable = input("Bitte gib den Wert für die Variable ein: ")
+
+# gebe den Wert der Variable aus
+print("Der Wert der Variablen ist:", variable)
+
+# zweites Beispiel
+name = input("Wie heißt du? ")
+print("Hallo, " + name + "!")
+print(f"Hallo, {name}!")
+print ("Hallo", name)
